c77a310fd58e4cd335c9f50c7e5ae65881c05e81
The Geek on Skates <geekonskates@protonmail.com>
2023-04-11 19:15:14 -0400

    0.2: Added the compiler, and a bunch more words

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+# TO-DO's for 0.2
+
+* Now that the compiler works, finish getting it to redefine words
+* And like I said, I'd like to replace the word `COMPILER` with `SEE`;
+* Beyond the compiler....
+	- If I'm gonna do `SEE`, may as well use the same technique to implement `CHAR`...
+	- I'd kinda like to add `."`, maybe `S"` and `COUNT` and fix `TYPE`
+	- Also, I could save a few bytes by adding `ABORT` - there are 3-4 places it would make sense to do that, but not a big deal
+* Update my docs :)
+
+
+--------------------------------------------------------------------------------
+
 # Pa'lante
-Version 0.1
+Version 0.2
 
 ## Overview
 
@@ -8,9 +21,10 @@ Pa'lante is a [Forth](https://www.forth.com/starting-forth/) interpreter for the
 * The Commodore PET (starting in version 0.0)
 * The VIC-20 (starting in version 0.1, requires 35K expansion, though if I could decrypt their config-language I might get it working on other VIC setups)
 * The C64 (starting in version 0.1)
-* The C128 (planned)
-* The Plus4 (planned)
-* The Commander X16 (planned)
+* The C128 (starting in version 0.2)
+* The Plus4 (starting in version 0.2)
+* The CBM-II (starting in version 0.2)
+* The Commander X16 (starting in version 0.2)
 * And when it's done, it might also be portable to other 8-bit systems (I'd kind of like to see Apple II and Atari builds, for example)
 
 My goal is to keep the memory size at under 16K.  Yes I could go lower, but the lower you go the less like Forth you get.  This Git repo is *NOT* my first attempt - oh, there have been many others - so I 've done my research.  I'm all too familiar with those who say you can build a Forth with just 3-4 instructions (`@` `!` and `EXECUTE`).  I could do that, easily, and it would run on just about anything.  But that's more like some kind of strange Assembly REPL than a Forth.  All you can do with that is store binary opcodes in memory and then jump to those addresses.  That's neat, but how do you define new words?  Where's the stack?  Where are `DUP` and `SWAP` and stuff that makes Forth... Forth?  Not that I expect Pa'lante to be fully ANS Forth standard compliant, not that it's gonna have a zillion words out of the box, but I want it to be something a programmer who knows Forth can look at and easily be able to pick up.
@@ -22,7 +36,17 @@ Forth has a rich history of cheesy puns.  With different variants having names l
 * "BreakForth" (cuz Breakaway Forth doesn't sound as cool as Breakaway BASIC - a project I may pick up again someday... or not :D)
 * "Skate Forth" (which might end up being my web-based Forth - tho I may not need/want one anymore cuz someone already [beat me to it](https://brendanator.github.io/jsForth/)).
 
-But I'm also fluent in Spanish, and especially familiar with Caribbean (Cuban, Puerto Rican etc.) Spanish slang.  So if you would ask me, how would you translate kingly-sounding phrases like "Let us go Forth" or "I shall go forth"... the translation is obvious.  In "proper" Spanish, Forth would be "para adelante".  But in some places, that get shortened to "para'delante" or (more commonly) just "pa'lante".  This of course reminds me of a few of my favorite songs: [Vamos Pa'lante!](https://www.youtube.com/watch?v=G7TDR9CpQOc), [Voy Pa'lante](https://www.youtube.com/watch?v=hK2wySMe_jc) and even [No Vuelvo Pa'tras](https://www.youtube.com/watch?v=Slng3XclDQ8) ("pa'tras" being the opposite of pa'lante - rough translation is "I'm not turning back").  Asi que vamos pa'lante! :)
+But I'm also fluent in Spanish, and especially familiar with Caribbean (Cuban, Puerto Rican etc.) Spanish slang.  So if you would ask me, how would you translate kingly-sounding phrases like "Let us go Forth" or "I shall go forth"... the translation is obvious.  In "proper" Spanish, Forth would be "para adelante".  But in some places, that get shortened to "pa'delante" or (more commonly) just "pa'lante".  This of course reminds me of a few of my favorite songs: [Vamos Pa'lante!](https://www.youtube.com/watch?v=G7TDR9CpQOc), [Voy Pa'lante](https://www.youtube.com/watch?v=hK2wySMe_jc) and even [No Vuelvo Pa'tras](https://www.youtube.com/watch?v=Slng3XclDQ8) ("pa'tras" being the opposite of pa'lante - rough translation is "I'm not turning back").  Asi que vamos pa'lante! :)
+
+## Road map to version 1.0
+
+The main things I want Pa'lante to have are:
+
+* **File I/O:** Planned for 0.3.  Technically already possible with binary opcodes Assembly and `EXECUTE`, but who wants to do that?  I'd like `LOAD`, `SAVE` and (bytes allowing) `VERIFY` like BASIC has.
+* **Branching:** Planned for 0.4.  Words like `IF` and `DO` and `BEGIN`
+
+Once it can do all these things, it'll be good enough to be considered a useful development tool.  Faster than BASIC, easier than Assembly, and it runs right on the machine.  At least, that's the dream. :D
+
 
 ## Available words
 
@@ -32,15 +56,39 @@ I'm not going to dive into how Forth works - the link in the "overview" section 
 
 If you know Forth, these do what you think they do. 😆  Note that they are case-sensitive - that is, they're lowercase if you're using the lowercase character set, or upper case if you're using that character set.  I used uppercase in my docs just as a convention, but by default they will be lowercase (not sure how I can fix that - it's a compiler thing).
 
-* **Memory:** `@` `!` `@` `!`
+* **Memory:** `C@` `C!` `@` `!`
 * **Math:** `+` `-` `*` `/` `MOD` `LSHIFT` `RSHIFT` `AND` `OR` `XOR` `INVERT` `NEGATE`
 * **Stack:** `DUP` `SWAP` `OVER` `.` `.S` `EMIT` `DROP` `PICK` `ROLL` `DEPTH`
 * **Comparison:** `=` `<>` `<` `<=` `>` `>=`
-* **Strings:** `TYPE`
-* **Misc:** `BYE` `EXECUTE`
+* **Strings:** `."` `S"` `COUNT` `TYPE`
+* **Misc:** `:` `;` `ACCEPT` `BYE` `EXECUTE` `NEXT`
+
+
+### Words I'd like to add in 0.3:
+
+* **The BIG goal:** TIme to take on file I/O.
+* Comments: `(` `)` and... `\` is a tricky one... Commodore keyboards don't have them... `#` maybe?  `//`?
+* `R>` and `>R`
+* `PAD` - push end of ms - 80 chars or something easy like that on the return stack
+* An escape mechanism:  In durexForth, I can hit RUN/STOP to exit an infinite loop; I'd kinda like to see if there's an interrupt I can listen for, and then if it's triggered set dsp and rsp to zero.
+
 
 
 ### Words only included in Pa'lante
 
-None yet - but I expect a `LOAD` and `SAVE` at least
+* None yet - but I expect a `LOAD` and `SAVE` at least.  Probably `S" MY-FILE,SEQ" 8 LOAD` and `S" @0:MY-FILE,SEQ" 8 SAVE` similar to BASIC.
 
+
+
+### Words that won't be written in native code, not available "out of the box"
+
+* I think the rest will be system-dependent and be loaded into disk files (so there'll probably be i.e. SID chip words for the C64 build, character set words for the VIC etc.).
+* And if I'm gonna do that, may as well add some more standard Forth words in disk files, like `2DROP` and `2SWAP` and `0=`
+
+
+
+### Words I don't plan to do anytime soon
+
+* `VARIABLE` and `CONSTANT`; because I can get the similar results with words that just push numbers on the stack (like `: RED 2 ;` and `: TEXT 646 ;` - I can do `RED TEXT C!` or `TEXT C@ .` like I would with a variable/constant).
+* `ALLOT` cuz without `VARIABLE` or `CONSTANT` having that word makes no sense
+* `INCLUDE`, because Commodore's disk API is not like standard Forths; like I said, `LOAD` and `SAVE` will probably work more like in BASIC.
--- main.c
+++ main.c
@@ -40,6 +40,7 @@
 
 #define INPUT_SIZE 80
 #define STACK_SIZE 256
+#define DICT_SIZE 4096
 
 
 // Status codes
@@ -48,7 +49,8 @@
 #define STATUS_COMPILED			1		// All words compiled successfully
 #define STATUS_STACK_UNDERFLOW	2		// Stack underflow error
 #define STATUS_DIV_BY_ZERO		3		// Division by 0 error
-
+#define STATUS_UNKNOWN_WORD		4		// Unknown word error
+#define STATUS_RS_UNDERFLOW		5		// Return stack underflow error
 
 
 // -----------------------------------------------------------------------------
@@ -56,11 +58,19 @@
 // -----------------------------------------------------------------------------
 
 int16_t ds[STACK_SIZE], dsp,	// Data stack and its pointer
-	rs[STACK_SIZE], rsp;		// Return stack and its pointer
-uint16_t goWhere;				// Used to make EXECUTE work
-uint8_t status = STATUS_OK;		// System status
-char input[INPUT_SIZE];			// User input buffer
+	rs[STACK_SIZE], rsp,		// Return stack and its pointer
+	goWhere;					// Used to make EXECUTE work
+uint8_t status = STATUS_OK,		// System status
+	ms[STACK_SIZE], msp;		// Memory stack (for strings)
+char input[INPUT_SIZE],			// User input buffer
+	redefined[81];				// If redefining a word
 char* ip;						// Interpreter pointer
+char dictionary[DICT_SIZE];		// The list of user-defined words
+bool compiling = false,			// Whether or not we're compiling
+	redefining = false,			// Whether or not we're redefining a word
+	isName = false;				// Set to true if the next word is the name of a
+								// word in a : definition
+
 
 
 // -----------------------------------------------------------------------------
@@ -307,14 +317,100 @@ void roll() {
 }
 
 void type() {
-	static char* string;
-	static int16_t j;
+	static int16_t i;
 	if (dsp < 2) { status = STATUS_STACK_UNDERFLOW; return; }
-	string = (char*)ds[dsp - 2];
-	for (j=0; j<ds[dsp - 1] * 2; j++) {
-		printf("%c", string[j]);
-	}
+	for (i=0; i<ds[dsp - 1]; i++)
+		printf("%c", PEEK(ds[dsp - 2] + i));
 	dsp -= 2;
+}
+
+void accept() {
+	static int8_t i;
+	
+	// Get user input
+	memset(input, 0, INPUT_SIZE);
+	fgets(input, INPUT_SIZE, stdin);
+	
+	// Convert \n or Shift-space to a space for easier parsing
+	for (i=0; i<INPUT_SIZE; i++)
+		if (input[i] == '\n' || input[i] == 160) input[i] = ' ';
+	
+	// Push all the words the user typed onto the return stack
+	for (i=INPUT_SIZE - 1; i>-1; i--) {
+		
+		// Move past any unset chars
+		while (i>0 && input[i] == '\0') i--;
+		
+		// Move past any trailing spaces
+		while (i>=0 && input[i] == ' ') i--;
+		
+		// Move to the space before the start of the word (if there is one)
+		while (i>=0 && input[i] != ' ') i--;
+		
+		// If there is one, move up 1 char to go to the start of the word
+		if (i == -1 || input[i] == ' ') i++;
+		
+		// And push it to the return stack
+		if (i >= 0 && input[i] != ' ') {
+			rs[rsp] = (uint16_t)(input + i);
+			rsp++;
+		}
+	}
+}
+
+void next() {
+	rsp--;
+	ip = (char*)rs[rsp];
+}
+
+void dotQuote() {
+	static uint8_t i;
+	if (!rsp) { status = STATUS_RS_UNDERFLOW; return; }
+	while(rs) {
+		next();
+		i = 0;
+		while(ip[i] != ' ') {
+			if (ip[i] == '"') return;
+			putchar(ip[i]);
+			i++;
+		}
+		putchar(' ');
+	}
+}
+
+void count() {
+	static uint16_t ptr;
+	if (!dsp) { status = STATUS_STACK_UNDERFLOW; return; }
+	ptr = ds[dsp - 1];
+	ds[dsp] = 0;
+	while(true) {
+		if (!PEEK(ptr)) break;
+		ds[dsp]++;
+		ptr++;
+	}
+	dsp++;
+}
+
+void sQuote() {
+	static uint8_t i;
+	if (!rsp) { status = STATUS_RS_UNDERFLOW; return; }
+	ds[dsp] = (uint16_t)ms + msp;
+	dsp++;
+	while(rs) {
+		next();
+		i = 0;
+		while(ip[i] != ' ') {
+			if (ip[i] == '"') break;
+			ms[msp] = ip[i];
+			msp++;
+			i++;
+		}
+		if (ip[i] == '"') break;
+		ms[msp] = ' ';
+		msp++;
+	}
+	ds[dsp] = (uint16_t)ms + msp - ds[dsp - 1];
+	dsp++;
 }
 
 
@@ -323,6 +419,52 @@ void type() {
 // -----------------------------------------------------------------------------
 // TO BE SORTED
 // -----------------------------------------------------------------------------
+
+/**
+ * At the beginning of the compile process, this deletes any previous definition
+ * of the word (kind of like FORGET in some Forths.  Hmmmm...) :D
+ */
+void clearCompiledWord() {
+	static char* start;
+	static uint16_t i = 0;
+	
+	// Build the start of the definition ("\n" + the name of the word)
+	memset(redefined, 0, 81);
+	redefined[0] = '\n';
+	i = 0; while (ip[i] == ' ') i++;
+	while(ip[i] == ' ') i++;
+	for (; i<81; i++) {
+		redefined[i + 1] = ip[i];
+		if (ip[i] == ' ') break;
+	}
+	
+	// Check if the word is already in the dictionary
+	start = strstr(dictionary, redefined);
+	if (start == NULL) return;
+	
+	// If it gets here, we ARE redefining a word
+	redefining = true;
+	
+	// Write over the old definition
+	start++;
+	while(start[0] != '\n') {
+		i = 0;
+		while(start[i] != '\0') {
+			start[i] = start[i + 1];
+			i++;
+		}
+	}
+	
+	// The code above leaves us with a double \n, which makes the "compiler"
+	// word a bit less useful and wasts bytes users could use for their code.
+	// So jump thru that hoop one more time (maybe make this an function in the
+	// future - but right now... IT WORKS!  YES!!!  Thank you Lord! :)
+	i = 0;
+	while(start[i] != '\0') {
+		start[i] = start[i + 1];
+		i++;
+	}
+}
 
 /**
  * Checks if a string contains a signed whole number
@@ -343,8 +485,57 @@ bool IsNumber(char* word) {
 	return true;
 }
 
+/**
+ * Checks if the current word is in the dictionary, and puts its definition on
+ * the return stack if it is
+ * @returns True if the word is in the dictionary, false if it isn't
+ */
+bool InDictionary() {
+	// Build a string: \n + current word + space
+	static char wordName[82], * temp;
+	static uint16_t i;
+	memset(wordName, 0, 82);
+	wordName[0] = '\n';
+	temp = ip; i = 0;
+	while(temp[0] != ' ' && temp[0] != '\n') {
+		wordName[i + 1] = temp[0];
+		i++; temp++;
+	}
+	i++;
+	wordName[i] = ' ';
+	
+	// If this combo is not found in the dictionary, do nothing
+	temp = strstr(dictionary, wordName);
+	if (temp == NULL) return false;
+	
+	// If it gets here, it IS in the dictionary, so push every word in its
+	// definition onto the return stack.  To get there, I need to do like I did
+	// in the main loop.  First, move to the end of the definition:
+	temp++;
+	while(temp[0] != '\n') temp++;	// End of the definition
+	temp--;		// so it's not at the closing \n anymore
+	
+	// Now again, like I did there, work backward till it reaches the word in ip
+	while(temp[0] != '\n') {
+		while(temp[0] == ' ') temp--;
+		while(temp[0] != ' ' && temp[0] != '\n') temp--;
+		if (temp[0] == '\n') break;
+		temp++;
+		rs[rsp] = (uint16_t)temp;
+		rsp++;
+		temp--;
+	}
+	
+	return true;
+}
+
 void main() {
-	static int8_t i;
+	static uint16_t length;
+	
+	// For now, I'm gonna put some test data in my dictionary.
+	// Once I have my code finding and running compiled words, then I'll add
+	// the : and ; words so users can add/edit (see my previous attempt for how)
+	strcpy(dictionary, "\nrot 2 roll \n-rot rot rot \n2dup over over \n2drop drop drop \n");
 	
 	// Set the background color to black and the text color to white
 	#if VIC20
@@ -359,46 +550,65 @@ void main() {
 	asm("LDA #1");
 	asm("STA 646");
 	#endif
-	printf("%cpa'lante 0.1\n\n", 147);
+	printf("%cpa'lante 0.2\n\n", 147);
 	while(true) {
-		// Get user input
-		memset(input, 0, INPUT_SIZE);
-		fgets(input, INPUT_SIZE, stdin);
-		
-		// Convert \n or Shift-space to a space for easier parsing
-		for (i=0; i<INPUT_SIZE; i++)
-			if (input[i] == '\n' || input[i] == 160) input[i] = ' ';
-		
-		// Push all the words the user typed onto the return stack
-		for (i=INPUT_SIZE - 1; i>-1; i--) {
-			
-			// Move past any unset chars
-			while (i>0 && input[i] == '\0') i--;
-			
-			// Move past any trailing spaces
-			while (i>=0 && input[i] == ' ') i--;
-			
-			// Move to the space before the start of the word (if there is one)
-			while (i>=0 && input[i] != ' ') i--;
-			
-			// If there is one, move up 1 char to go to the start of the word
-			if (i == -1 || input[i] == ' ') i++;
-			
-			// And push it to the return stack
-			if (i >= 0 && input[i] != ' ') {
-				rs[rsp] = input + i;
-				rsp++;
-			}
-		}
+		// Parse user input
+		accept();
 		
 		// Run it, one word at a time
-		status = STATUS_OK;
+		if (!compiling) status = STATUS_OK;
 		while(rsp > 0) {
+			// If the status is not OK or COMPILED, exit the loop
+			if (status > STATUS_COMPILED) {
+				dsp = 0;
+				rsp = 0;
+				break;
+			}
+			
 			// Pop the return stack to the input pointer
-			rsp--;
-			ip = (char*)rs[rsp];
+			next();
+			
+			// If we're compiling, keep compiling unless it's a semicolon
+			if (compiling) {
+				if (isName) clearCompiledWord();
+				isName = false;
+				if (StringStartsWith(ip, "; ")) {
+					strcat(dictionary, "\n");
+					compiling = false;
+					status = STATUS_OK;
+					if (redefining) {
+						redefined[0] = ' ';
+						printf("redefined%s\n", redefined);
+					}
+					redefining = false;
+				}
+				else {
+					length = strlen(dictionary);
+					while(ip[0] != ' ') {
+						dictionary[length] = ip[0];
+						length++;
+						ip++;
+					}
+					dictionary[length] = ' ';
+				}
+				continue;
+			}
+			
+			// This is not a standard Forth word, but a tutorial I followed once
+			// (the one I learned this compiler strategy from) had it and I like
+			// it, so it works for now (lol).  Maybe I'll replace it with "SEE"
+			// later... or maybe I'll just leave it :)
+			if (StringStartsWith(ip, "compiler ")) {
+				printf("\n%s", dictionary);
+				continue;
+			}
 
 			// Handle Forth words
+			if (StringStartsWith(ip, ": ")) {
+				compiling = isName = true;
+				status = STATUS_COMPILED;
+				continue;
+			}
 			if (StringStartsWith(ip, "= ")) {
 				equals();
 				continue;
@@ -439,6 +649,10 @@ void main() {
 				printStack();
 				continue;
 			}
+			if (StringStartsWith(ip, ".\" ")) {
+				dotQuote();
+				continue;
+			}
 			if (StringStartsWith(ip, "+ ")) {
 				add();
 				continue;
@@ -455,8 +669,8 @@ void main() {
 				divide();
 				continue;
 			}
-			if (StringStartsWith(ip, "mod ")) {
-				mod();
+			if (StringStartsWith(ip, "accept ")) {
+				accept();
 				continue;
 			}
 			if (StringStartsWith(ip, "and ")) {
@@ -472,6 +686,10 @@ void main() {
 			}
 			if (StringStartsWith(ip, "c@ ")) {
 				cfetch();
+				continue;
+			}
+			if (StringStartsWith(ip, "count ")) {
+				count();
 				continue;
 			}
 			if (StringStartsWith(ip, "depth ")) {
@@ -502,8 +720,16 @@ void main() {
 				lshift();
 				continue;
 			}
+			if (StringStartsWith(ip, "mod ")) {
+				mod();
+				continue;
+			}
 			if (StringStartsWith(ip, "negate ")) {
 				negate();
+				continue;
+			}
+			if (StringStartsWith(ip, "next ")) {
+				mod();
 				continue;
 			}
 			if (StringStartsWith(ip, "or ")) {
@@ -526,6 +752,10 @@ void main() {
 				rshift();
 				continue;
 			}
+			if (StringStartsWith(ip, "s\" ")) {
+				sQuote();
+				continue;
+			}
 			if (StringStartsWith(ip, "swap ")) {
 				swap();
 				continue;
@@ -546,6 +776,12 @@ void main() {
 				continue;
 			}
 			
+			// If it's not a number, is it in the dictionary?
+			if (InDictionary()) continue;
+			
+			// If it's not in the dictionary, it' an error
+			status = STATUS_UNKNOWN_WORD;
+			
 			// If at any point there's an error, stop running
 			if (status > STATUS_COMPILED) {
 				dsp = 0;
@@ -556,9 +792,14 @@ void main() {
 		
 		// And print the system status
 		switch(status) {
-			case STATUS_COMPILED: printf("compiled"); break;
+			case STATUS_COMPILED: printf("  compiled"); break;
 			case STATUS_STACK_UNDERFLOW: printf("stack underflow"); break;
 			case STATUS_DIV_BY_ZERO: printf("division by zero"); break;
+			case STATUS_RS_UNDERFLOW: printf("return stack underflow"); break;
+			case STATUS_UNKNOWN_WORD:
+				strchr(ip, ' ')[0] = '\0';
+				printf("unknown word \"%s\"", ip);
+				break;
 			default: printf("  ok");
 		}
 		printf("\n");
